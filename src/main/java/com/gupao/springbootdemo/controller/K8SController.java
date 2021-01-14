4adc94a697ad616df78904c959becd118a64189a
lp_lap_glb <lp_lap_glb@88.com>
2021-01-15 00:44:21 +0800

    k8s测试1

--- src/main/java/com/gupao/springbootdemo/controller/K8SController.java
+++ src/main/java/com/gupao/springbootdemo/controller/K8SController.java
@@ -14,6 +14,6 @@ public class K8SController {
      **/
     @RequestMapping("/k8s")
     public String k8s(){
-        return "hello K8s <br/>12233 ";
+        return "hello K8s <br/>1233 ";
     }
 }
